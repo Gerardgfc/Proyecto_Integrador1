05b84c0ae99b218722fcafa69231e9bc9ed8da2f
Gerardgfc <gerardo.carrizo123.gc@gmail.com>
2023-12-30 02:25:14 -0300

    6° commit main

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 import pandas as pd
-import numpy as np
-from sklearn.metrics.pairwise        import cosine_similarity
-from sklearn.metrics.pairwise        import linear_kernel
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = FastAPI(debug=True)
 
 df = pd.read_csv("Csv/Df_Merged.csv.gz", compression='gzip', encoding='utf-8')
 
+def get_genre_data(genre: str):
+    genre = genre.capitalize()
+    if genre not in df.columns:
+        return None
+    return df[df[genre] == 1]
 
 @app.get("/")
 def Index():
@@ -16,97 +17,55 @@ def Index():
 
 @app.get("/PlayTimeGenre")
 def PlayTimeGenre(genre: str) -> dict:
-    """ 
-    Obtiene el año con más horas jugadas para un género específico. 
-
-    Genre: Genero a obtener el número de años de lanzamiento con más horas
-
-    Returns:
-        Año con más horas jugadas para un género específico teniendo en cuenta los generos disponibles.
-    """
-    genre = genre.capitalize()
-    if genre not in df.columns:
+    genre_df = get_genre_data(genre)
+    if genre_df is None:
         return {"Error": f"Género {genre} no encontrado en el dataset."}
-    else:
-        genre_df = df[df[genre] == 1]
-        year_playtime_df = genre_df.groupby('posted_year')['playtime_forever'].sum().reset_index()
-        max_playtime_year = year_playtime_df.loc[year_playtime_df['playtime_forever'].idxmax(), 'posted_year']
-        return {"Género": genre, f"Año de lanzamiento con más horas jugadas para Género {genre} :": int (max_playtime_year)} 
+    year_playtime_df = genre_df.groupby('posted_year')['playtime_forever'].sum().reset_index()
+    max_playtime_year = year_playtime_df.loc[year_playtime_df['playtime_forever'].idxmax(), 'posted_year']
+    return {"Género": genre, f"Año de lanzamiento con más horas jugadas para Género {genre} :": int(max_playtime_year)}
 
 @app.get("/UserForGenre")
 def UserForGenre(genre: str) -> dict:
-    """Devuelve el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
-    
-    Genre: Genero a obtener el número de usuarios que han jugado
-
-    Returns:
-        Usuario que ha jugado más horas para un género específico teniendo en cuenta los generos disponibles.
-    """
-    genre = genre.capitalize()
-    if genre not in df.columns:
+    genre_df = get_genre_data(genre)
+    if genre_df is None:
         return {"Error": f"Género {genre} no encontrado en el dataset."}
-    else:
-        genre_df = df[df[genre] == 1]
-        user_playtime_df = genre_df.groupby('user_id')['playtime_forever'].sum().reset_index()
-        max_playtime_user = user_playtime_df.loc[user_playtime_df['playtime_forever'].idxmax(), 'user_id']
-        return {"Género": genre, f"Usuario con más horas jugadas para Género {genre} :": max_playtime_user}
+    user_playtime_df = genre_df.groupby('user_id')['playtime_forever'].sum().reset_index()
+    max_playtime_user = user_playtime_df.loc[user_playtime_df['playtime_forever'].idxmax(), 'user_id']
+    return {"Género": genre, f"Usuario con más horas jugadas para Género {genre} :": max_playtime_user}
 
+def get_year_data(year: int):
+    if year < 2010 or year > 2015:
+        return None
+    return df[df['posted_year'] == year]
 
 @app.get("/UsersRecommend")
-def UsersRecommend( year : int ):
-    """Devuelve el top 3 de juegos más recomendados por usuarios para el año dado.
-
-    Year: Año a obtener los juegos más recomendados
-
-    Returns:
-        Lista con los juegos más recomendados para el año dadoy teniendo en cuenta los años que disponemos.
-    """
-    if year < 2010 or year > 2015:
+def UsersRecommend(year: int):
+    year_df = get_year_data(year)
+    if year_df is None or year_df.empty:
         return {"Año": year, "Juegos más recomendados para el año dado": "No hay datos para el año dado"}
-    else:
-        year_df = df[df['posted_year'] == year]
-        year_df = year_df.groupby('item_name')['sentiment_score'].sum().reset_index()
-        year_df = year_df.sort_values(by='sentiment_score', ascending=False)
-        year_df = year_df.head(3)
-        return {"Año": year, "Juegos más recomendados para el año dado": year_df['item_name'].tolist()}
-    
+    year_df = year_df.groupby('item_name')['sentiment_score'].sum().reset_index()
+    year_df = year_df.sort_values(by='sentiment_score', ascending=False)
+    year_df = year_df.head(3)
+    return {"Año": year, "Juegos más recomendados para el año dado": year_df['item_name'].tolist()}
+
 @app.get("/UsersWorstDeveloper")
-def UsersWorstDeveloper( year : int ):
-    """Devuelve el top 3 de juegos menos recomendados por usuarios para el año dado.
-
-    Year: Año a obtener los juegos menos recomendados
-
-    Returns:
-        Lista con los juegos menos recomendados para el año dado y teniendo en cuenta los años que disponemos.
-    """
-    if year < 2010 or year > 2015:
+def UsersWorstDeveloper(year: int):
+    year_df = get_year_data(year)
+    if year_df is None or year_df.empty:
         return {"Año": year, "Juegos menos recomendados para el año dado": "No hay datos para el año dado"}
-    else:
-        year_df = df[df['posted_year'] == year]
-        year_df = year_df.groupby('item_name')['sentiment_score'].sum().reset_index()
-        year_df = year_df.sort_values(by='sentiment_score', ascending=True)
-        year_df = year_df.head(3)
-        return {"Año": year, "Juegos menos recomendados para el año dado": year_df['item_name'].tolist()}
+    year_df = year_df.groupby('item_name')['sentiment_score'].sum().reset_index()
+    year_df = year_df.sort_values(by='sentiment_score', ascending=True)
+    year_df = year_df.head(3)
+    return {"Año": year, "Juegos menos recomendados para el año dado": year_df['item_name'].tolist()}
 
 @app.get("/sentiment_analysis")
-def sentiment_analysis( developer: str ):
-    """Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
-    
-    Developer: Empresa desarrolladora
-    
-    Returns:
-        Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}, en caso de que no se encuentre ningúna desarrolladora dara como rsultado error.
-    """
+def sentiment_analysis(developer: str):
     df['developer'] = df['developer'].str.capitalize()
     developer = developer.capitalize()
     if developer not in df['developer'].values:
         return {developer: ["No está registrado"]}
-    else:
-        developer_df = df[df['developer'] == developer]
-    
-        negative = len(developer_df[developer_df['sentiment_score'] == 0])
-        neutral = len(developer_df[developer_df['sentiment_score'] == 1])
-        positive = len(developer_df[developer_df['sentiment_score'] == 2])
-
-        return {developer: ["Negative:",negative, "Neutral:", neutral, "Positive:", positive]}
-    
+    developer_df = df[df['developer'] == developer]
+    negative = len(developer_df[developer_df['sentiment_score'] == 0])
+    neutral = len(developer_df[developer_df['sentiment_score'] == 1])
+    positive = len(developer_df[developer_df['sentiment_score'] == 2])
+    return {developer: ["Negative:", negative, "Neutral:", neutral, "Positive:", positive]}
